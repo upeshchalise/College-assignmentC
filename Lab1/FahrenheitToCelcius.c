e27ef3edcea019305255cc5c161bb647aea1e455
upeshchalise <upeshchalise@gmail.com>
2023-01-03 20:03:59 +0545

    added

--- Lab1/FahrenheitToCelcius.c
+++ Lab1/FahrenheitToCelcius.c
@@ -2,3 +2,10 @@
 
 //WAP to input temperature in fahrenheit and convert into celsius.
 
+int main() {
+    float F, c;
+    printf("Input the temperature in fahernheit: ");
+    scanf("%f", &F );
+    c = (F-32.0) * (5.0/9.0);
+    printf("The temperature in celcius is %.2f\n",c);
+}
